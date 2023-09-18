693ceb2f5d5670ada4ed79fc86053c7f406dd40d
duckien112 <duckien112@gmail.com>
2023-09-18 14:09:52 +0700

    Update seedsEncryption.py

--- keys/seedsEncryption.py
+++ keys/seedsEncryption.py
@@ -2,10 +2,38 @@
 #password = '1234'
 #seeds = 'a1 b2 c3 d4 e5 f6 g7 h8 i9 j10 k11 l12'
 from lib import *
+import getpass
 
-action = input("If encrypt enter 1, else 0: ")
-number = input("Enter number with 9 chars in Visa: ")
-password = input("Enter word with 9 chars in Visa: ")
-seeds = input("Enter seeds: ")
+#action = getpass.getpass("If encrypt enter 1, else 0: ")
+while True:
+    action = getpass.getpass("If encrypt enter 1, else 0: ")
+    if action == "0" or action == "1":
+        break
+    else:
+        print("Only 0 or 1")
+        
+if action == "1":
+    while True:
+        number = getpass.getpass("Enter number with 9 chars in Visa: ")
+        number1 = getpass.getpass("Renter number with 9 chars in Visa: ")
+
+        if number == number1:
+            break
+        else:
+            print("Number not match!!!")
+            
+    while True:
+        password = getpass.getpass("Enter word with 9 chars in Visa: ")
+        password1 = getpass.getpass("Renter word with 9 chars in Visa: ")
+
+        if password == password1:
+            break
+        else:
+            print("Password not match!!!")
+else:
+    number = getpass.getpass("Enter number with 9 chars in Visa: ")
+    password = getpass.getpass("Enter word with 9 chars in Visa: ")
+    
+seeds = getpass.getpass("Enter seeds: ")
 
 print(encryptSeeds(action, number, password, seeds))
