14a0acab3805919a79b850375b83f2c54c39bd29
duckien112 <duckien112@gmail.com>
2023-09-18 14:08:50 +0700

    Update changeSeeds.py

--- keys/changeSeeds.py
+++ keys/changeSeeds.py
@@ -2,9 +2,14 @@
 #password = '1234'
 #seeds = 'a1 b2 c3 d4 e5 f6 g7 h8 i9 j10 k11 l12'
 from lib import *
+import getpass
 
-action = input("If encrypt enter 1, else 0: ")
-number = input("Enter number with 9 chars in Visa: ")
-seeds = input("Enter seeds: ")
+#action = input("If encrypt enter 1, else 0: ")
+#number = input("Enter number with 9 chars in Visa: ")
+#seeds = input("Enter seeds: ")
+
+action = getpass.getpass("If encrypt enter 1, else 0: ")
+number = getpass.getpass("Enter number with 9 chars in Visa: ")
+seeds = getpass.getpass("Enter seeds: ")
 
 print(changeSeeds(seeds, number, action))
